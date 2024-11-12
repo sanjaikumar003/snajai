aecb4dceff85da4c5cebe8e737204542693eff40
sanja <sanja@sanjaikumar003.lan>
2024-11-12 23:29:44 +0530

    commit

---
+++ C2TC_COREJAVA/src/day2/Customer.java
@@ -0,0 +1,44 @@
+package day2;
+
+public class Customer {
+
+	
+		private int cid;
+		private String cname;
+		private String city;
+		private String address;
+		public void setCid(int cid)
+		{
+			this.cid=cid;
+		}
+		public void setCname(String cname)
+		{
+			this.cname=cname;
+		}
+		public void setCity(String city)
+		{
+			this.city=city;
+		}
+		public void setAddress(String address)
+		{
+			this.address=address;
+		}
+		public int getCid() {
+			return this.cid;
+		}
+		public String getCname()
+		{
+			return this.cname;
+		}
+		public String getCity()
+		{
+			return this.city;
+		}
+		public String getAddress()
+		{
+			return this.address;
+		}
+
+	
+
+}
---
+++ C2TC_COREJAVA/src/day2/CustomerDemo.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		Customer c2=new Customer();
+		Customer c3=new Customer();
+		Customer c4=new Customer();
+		Customer c5=new Customer();
+		c1.setCid(221);
+		c1.setCname("sanjai");
+		c1.setCity("sm");
+		c1.setAddress("north street");
+
+	}
+
+}
